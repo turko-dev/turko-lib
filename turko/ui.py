1b5ac0c8c35c72d9d94b33e13158ad8431847075
turko-dev <turkoai@protonmail.com>
2024-11-09 14:00:32 +0000

    implemented after() function and resizing based on percentage works now. Just need to add input validation now

--- turko/ui.py
+++ turko/ui.py
@@ -1,13 +1,21 @@
 from tkinter import ttk
 class Frame:
     def __init__(self, parent, width, height):
+        
         self.parent = parent
         self.frame = ttk.Frame(master=parent.root, width=width, height=height)
         self.parent.root.bind("<Configure>", self.test, add=True)
-        self.frame.pack_propagate(True)
+        self.frame.config(width=100, height=100)
         self.frame.pack()
+        self.frame.pack_propagate(False)
+
+
 
     def test(self, event):
-        self.frame.config(width=event.width * 0.9, height=event.height * 0.9)
+        self.width = event.width * 0.9
+        self.height = event.height * 0.9
+        print(event)
+        if(event.widget == self.parent.root):
+            self.frame.after(5 ,self.frame.config(width=self.width, height=self.height))
 
 #IMPLEMENT AFTER() function somewhere here
