55aa27128bbb2f1240aeea057ed960b0593a1c6d
turko-dev <turkoai@protonmail.com>
2024-12-12 15:52:37 +0000

    clean frame class

--- turko/ui.py
+++ turko/ui.py
@@ -52,27 +52,17 @@ class Font:
 
 class Frame:
     def __init__(self, parent, width, height, styleName, bg="#FFFFFF", borderwidth=0):
+        
+        #Frame Parent & Children Configuration
+        self.parent = parent
+        self.childCount = 0       
+        
+        #Frame Content & Item Justification & Alignment
         self.contentJustification = "evenly" #even justification is default
         self.itemAlignment = "center" #center is default
-        self.childCount = 0
-        """
-        there are only 4 cases
-        #1 - only width is percentage (100%, 500px)
-        #2 - only height is percentage (500px, 100%)
-        #3 - both width and height is percentage (100%, 50%)
-        #4 - non are percentage (500px, 500px)
-        """
-        
-        #Type Validation
-        if not (isinstance(width, str) or isinstance(width, int)): raise TypeError("width attribute must be either a string percentage or a integer pixel value such as \"100%\" or 100")
-        if not (isinstance(height, str) or isinstance(height, int)): raise TypeError("height attribute must be either a string percentage or a integer pixel value such as \"100%\" or 100")
-        if not (isinstance(bg, str)): raise TypeError("backgroundColor attribute must be a string such as \"#FFFFFF\"")
-        self.parent = parent
         
         
-        #Styling
-        self.backgroundColor = bg
-        
+#Resize Case 1 Binding
         if(isinstance(width, str) and (isinstance(height, int))):
             percentage_string = []
             self.percentage_width = ""
@@ -83,7 +73,7 @@ class Frame:
             self.pwidth = int(self.percentage_width)
             self.pheight = height
             self.parent.root.bind("<Configure>", self._resize_case_1, add=True)
-
+#Resize Case 2 Binding
         elif(isinstance(height, str) and isinstance(width, int)):
             percentage_string = []
             self.percentage_height = ""
@@ -94,7 +84,7 @@ class Frame:
             self.pheight = int(self.percentage_height)
             self.pwidth = width
             self.parent.root.bind("<Configure>", self._resize_case_2, add=True)
-
+#Resize Case 3 Binding
         elif(isinstance(width, str) and isinstance(height, str)):
             percentage_string_1, percentage_string_2 = [], []
             self.percentage_height, self.percentage_width = "", ""
@@ -107,46 +97,54 @@ class Frame:
             self.pwidth = int(self.percentage_width)
             self.pheight = int(self.percentage_height)
             self.parent.root.bind("<Configure>", self._resize_case_3, add=True)
-
+#Resize Case 4 Binding
         else:
             self.pwidth = width
             self.pheight = height
             self.parent.root.bind("<Configure>", self._resize_case_4, add=True)
 
 
+        #Frame Styling
+        self.borderWidth = borderwidth
+        self.backgroundColor = bg
         frameStyle = ttk.Style()
-        frameStyle.configure(f"{styleName}.TFrame", borderwidth=borderwidth, background=bg, relief="raised") 
-     
+        frameStyle.configure(f"{styleName}.TFrame", borderwidth=self.borderWidth, background=self.bg, relief="raised") 
+        
+
+        #Frame Init
         self.root = ttk.Frame(master=parent.root, width=0, height=0, style=f"{styleName}.TFrame")
         self.root.update_idletasks()
         self.root.pack_propagate(False)
-        
-
         self.root.pack()
         
 
-
+    
+    #Resize Case 1
     def _resize_case_1(self, event):
         self.width = event.width * (self.pwidth / 100)
         self.height = self.pheight
         if(event.widget == self.parent.root):
             self.root.after(0 ,self.root.config(width=self.width, height=self.height))
     
+    #Resize Case 2
     def _resize_case_2(self, event):
         self.width = self.pwidth
         self.height = event.height * (self.pheight / 100)
         if(event.widget == self.parent.root):
             self.root.after(0 ,self.root.config(width=self.width, height=self.height))
     
+    #Resize Case 3
     def _resize_case_3(self, event):
         self.width = event.width * (self.pwidth / 100)
         self.height = event.height * (self.pheight / 100)
         if(event.widget == self.parent.root):
             self.root.after(0 ,self.root.config(width=self.width, height=self.height))
 
+    #Resize Case 4
     def _resize_case_4(self, event):
         self._justification = np.round(np.linspace(0, self.pwidth, self.childCount + 2))
         self.root.after(0, self.root.config(width=self.pwidth, height=self.pheight))
 
+    #Return String Format
     def __str__(self):
         return "frame"
